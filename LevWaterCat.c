e48d0330aa8e475f6e69333f145a7d7ee8bfd551
Ho-mmd <ws000617@gmail.com>
2022-12-28 22:24:29 +0900

    Fin

--- LevWaterCat.c
+++ LevWaterCat.c
@@ -1,201 +1,129 @@
-#include<avr/io.h>
-#include<avr/interrupt.h>
-#include<util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/delay.h>
 
-#define ENABLE PORTE |= 0x01;
-#define DISABLE PORTE &= ~0x01;
+volatile unsigned int flag = 0, ADC_ReLED; //LED_Freq Variable
+volatile unsigned char ADC_LLED, ADC_HLED;
+volatile unsigned int ADC_ReWLev, Lev3; //Water_Level Variable
+volatile unsigned char ADC_LWLev, ADC_HWLev; 
+volatile unsigned int Tx; //Transmit Data
 
-#define RS_SET PORTE |= 0x04;
-#define RS_CLI PORTE &= ~0x04;
+void ADC_LED(); //LED Control
+void ADC_WaterLevel(); //Measure Water Level
 
-#define RW_SET PORTE |= 0x02;
-#define RW_CLI PORTE &= ~0x02;
+int main(void) {
+	
+	cli();
 
-volatile unsigned int measure, flag = 1, cnt = 0;
-void LED_CLK();
+	DDRF = 0x03; 
+	DDRB = 0x10;
+	DDRD = 0xF0;
+	DDRE = 0x02;	
+	
+	//PWM, Inverting Mode
+	//Set compare match: up_counting, Clear compare match: down_counting
+	TCCR0 = (0<<FOC0) | (1<<WGM00) | (1<<COM01) | (1<<COM00) | (0<<WGM01) | (0<<CS02) | (0<<CS01) | (1<<CS00 ); 
 
-void write_instruction(unsigned char data)
-{
-   RS_CLI
-   RW_CLI
-   ENABLE
-   _delay_us(1);
-   PORTB = data;
-   _delay_us(1);
-   DISABLE
-}
-
-void write_data(unsigned char data)
-{
-   RS_SET
-   RW_CLI
-   ENABLE
-   PORTB = data;
-   _delay_us(1);
-   DISABLE
-   _delay_us(1);
-}
-
-void init_LCD(void)
-{
-   _delay_ms(75);
-   write_instruction(0x30);
-   _delay_ms(25);
-   write_instruction(0x30);
-   _delay_ms(5);
-   write_instruction(0x30);
-   _delay_ms(5);
-   write_instruction(0x38);
-   _delay_ms(5);
-   write_instruction(0x08);
-   _delay_ms(5);
-   write_instruction(0x01);
-   _delay_ms(5);
-   write_instruction(0x04);
-   _delay_ms(5);
-   write_instruction(0x0c);
-   _delay_ms(5);
-}
-
-int main(void)
-{
-   cli();
-   
-   DDRD = 0x08; //PD3 출력
-   DDRE = 0x07; //PE0,1,2 출력
-   DDRB = 0xFF; //PB 전체 출력
-   DDRF = 0xFF;
-
-   UCSR1C = 0x07;
-   UCSR1B = 0x98;
-
-   UBRR1H = 0x00;
-   UBRR1L = 0x2F;   
-
-   _delay_ms(100);
-   init_LCD();   
-   
-   write_instruction(0x8B); _delay_ms(10);
-   write_data(':'); _delay_ms(100);
-   write_data('L'); _delay_ms(100);
-   write_data('E'); _delay_ms(100);
-   write_data('V'); _delay_ms(100);
-   write_data('E'); _delay_ms(100);
-   write_data('L'); _delay_ms(100);
-   write_data(' '); _delay_ms(100);
-   write_data('R'); _delay_ms(100);
-   write_data('E'); _delay_ms(100);
-   write_data('T'); _delay_ms(100);
-   write_data('A'); _delay_ms(100);
-   write_data('W'); _delay_ms(100);
-
-   	TIMSK = 0x04;
+	TIMSK = 0x04; //Timer_counter1 interrupt enabled
 	TCCR1A = 0x00; 
-	TCCR1B = 0x05; //clk select
+	TCCR1B = 0x05; //Prescale 1024
 
-	TCNT1H = 0xFF; //초기에 overflow 발생시기 당김
+	TCNT1H = 0xFF; //Advance the Overflow Time
 	TCNT1L = 0xC0;
 
-   sei();
+	ADMUX = 0x00; // ADC init()
+	ADCSRA = 0x83; // ADC Enable, Prescale 8
 
-   do{
+	UCSR0C = 0x07; //Stop bit: 1 bit, Data_Size: 8 bit, Transmit_data: Falling Edge, Received_data: Rising Edge
+	UCSR0B = 0x98; //Rx complete interrupt enable, Receiver & Transimitter Enable
 
-   }while(1);
-}
+	UBRR0H = 0x00; //Baud Rate
+	UBRR0L = 0x67;
 
-ISR(USART1_RX_vect)
-{
-   cli();
-   measure = UDR1;
+	sei();
+	
+	do {
+		
+	} while (1);
 
-   _delay_ms(100);
- 
-   if(measure == 0){
-	   write_instruction(0xCF); _delay_ms(10);
-	   write_data('%'); _delay_ms(100);
-	   write_data('0'); _delay_ms(100);
-	   write_data(' '); _delay_ms(100);
-	   write_data(' '); _delay_ms(100);
-	   /*PORTF = 0xFF;
-	   _delay_ms(100);
-	   PORTF = 0x00;
-	   _delay_ms(100);
-	   PORTF = 0xFF;
-	   _delay_ms(100);
-	   PORTF = 0x00;
-	   _delay_ms(100);
-	   PORTF = 0xFF;
-	   _delay_ms(100);
-	   PORTF = 0x00;
-	   _delay_ms(100);*/
-   }
-
-   else if(measure <= 2){
-	    write_instruction(0xCF); _delay_ms(10);
-	    write_data('%'); _delay_ms(100);
-	    write_data('5'); _delay_ms(100);
-	    write_data('2'); _delay_ms(100);
-	    write_data(' '); _delay_ms(100);
-   }
-
-   else if(measure <= 4){
-	   write_instruction(0xCF); _delay_ms(10);
-	   write_data('%'); _delay_ms(100);
-	   write_data('0'); _delay_ms(100);
-	   write_data('5'); _delay_ms(100);
-	   write_data(' '); _delay_ms(100);
-   }
-
-   else if(measure <= 6){
-	   write_instruction(0xCF); _delay_ms(10);
-	   write_data('%'); _delay_ms(100);
-	   write_data('5'); _delay_ms(100);
-	   write_data('7'); _delay_ms(100);
-	   write_data(' '); _delay_ms(100);
-   }
-
-   else if(measure > 6){
-	   write_instruction(0xCF); _delay_ms(10);
-	   write_data('%'); _delay_ms(100);
-	   write_data('0'); _delay_ms(100);
-	   write_data('0'); _delay_ms(100);
-	   write_data('1'); _delay_ms(100);
-   }
-
- 	LED_WaterLev(measure);
-
-   sei();
+	return 0;
 }
 
 
 ISR(TIMER1_OVF_vect) { 
 	cli();
 
-	TCNT1H = 0xFF; //0.004초
+	TCNT1H = 0xFF; //0.01초 (10ms)
 	TCNT1L = 0x62;
 
-	if(measure == 0) {
-		if(flag) {
-			PORTF = 0xFF;
-			flag = 0;
-		} else {		
-			PORTF = 0x00;
-			flag = 1;
-		}
-	}
+	ADC_LED(); //ADC_ to control LED Frequency
+	ADC_WaterLevel(); //ADC_ to receive Water level
 	
-		
+	OCR0 = 0x0F; //PWM Water Pump Control
+
+
+	while((UCSR0A& 0x20)==0); //Ready to receive new data
+
+	UDR0 = Tx; //Water Level Transmit
+
 	sei();
 
 }
 
-void LED_WaterLev(unsigned int a) {
-	if(0 < a && a <= 2)
-		PORTF = 0x01;
-	else if(a <= 4)
-		PORTF = 0x03;
-	else if(a <= 6)
-		PORTF = 0x07;
-	else if(a > 6)
-		PORTF = 0x0F;
+void ADC_LED() {
+	ADMUX = 0x00; //Use ADC Pin 0
+	ADCSRA |= 0x40; //ADC start, (1<<ADSC)
+
+	while(ADCSRA&0x40); //Wait until ADCSRA & 0x40 != 0, (1<<ADSC)
+
+	ADC_LLED = ADCL;  //Low value First
+	ADC_HLED = ADCH;
+	ADC_ReLED = (ADC_LLED + ADC_HLED * 256) / 20;
+
+	flag++;
+
+	if(flag < ADC_ReLED) {
+		PORTD = 0xF0;
+	} else {
+		PORTD = 0x00;
+		flag = 0;
+	}
+}
+
+void ADC_WaterLevel() {
+	ADMUX = 0x01; //Use ADC Pin 1
+	ADCSRA |= 0x40; //ADC start, (1<<ADSC)
+
+	while ((ADCSRA & 0x10) == 0); //Wait until ADCSRA & 0x40 != 0, (1<<ADSC)
+
+	ADC_LWLev = ADCL; //Low value First
+	ADC_HWLev = ADCH;
+	ADC_ReWLev = ADC_HWLev * 256 + ADC_LWLev;
+
+	Lev3 = ADC_ReWLev;
+
+	if ((Lev3) < 100) {
+		Tx = 0;
+	}
+	else if ((Lev3) >= 100 && (Lev3) < 150) {
+		Tx = 1;
+	}
+	else if ((Lev3) >= 150 && (Lev3) < 200) {
+		Tx = 2;
+	}
+	else if ((Lev3) >= 200 && (Lev3) < 250) {
+		Tx = 3;
+	}
+	else if ((Lev3) >= 250 && (Lev3) < 300) {
+		Tx = 4;
+	}
+	else if ((Lev3) >= 300 && (Lev3) < 350) {
+		Tx = 5;
+	}
+	else if ((Lev3) >= 350 && (Lev3) < 400) {
+		Tx = 6;
+	}
+	else if((Lev3 >= 400)){
+		Tx = 7;
+	}
 }
